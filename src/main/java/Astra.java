267e7f6c5c0856e186b0f82512571a284e9e496d
hsinyilow <hsinyilow@gmail.com>
2025-01-21 12:21:19 +0800

    Chatbot echos command entered by user, exits using command "bye"

--- src/main/java/Astra.java
+++ src/main/java/Astra.java
@@ -1,5 +1,6 @@
-public class Astra {
+import java.util.Scanner;
 
+public class Astra {
     public static void Greet(){
         System.out.println("Hello! I'm Astra ^-^");
         System.out.println("What can I do for you?");
@@ -10,7 +11,24 @@ public class Astra {
     }
 
     public static void main(String[] args) {
+        //initialize
+        Echo e = new Echo();
+        Scanner scanner = new Scanner(System.in);
+
         Greet();
-        End();
+
+        //chat loop
+        while(true){
+            String nextLine = scanner.nextLine();
+
+            //end
+            if(nextLine.equals("bye")){
+                End();
+                break;
+            }
+            e.Echoing(nextLine);
+        }
+
+        scanner.close();
     }
 }
---
+++ src/main/java/Echo.java
@@ -0,0 +1,6 @@
+public class Echo {
+
+    public void Echoing(String input){
+        System.out.println(input);
+    }
+}
