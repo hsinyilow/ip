c97089553e98ef063c61570ba213641ad4957938
hsinyilow <hsinyilow@gmail.com>
2025-01-21 11:59:55 +0800

    Removed default Duke, renamed Duke to Astra. Added in greeting and exit.

---
+++ src/main/java/Astra.java
@@ -0,0 +1,16 @@
+public class Conner {
+
+    public static void Greet(){
+        System.out.println("Hello! I'm Astra ^-^");
+        System.out.println("What can I do for you?");
+    }
+
+    public static void End(){
+        System.out.println("Bye. Hope to see you again soon! :)");
+    }
+
+    public static void main(String[] args) {
+        Greet();
+        End();
+    }
+}
