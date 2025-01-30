410cbe07b0584bf26a259582c7740318d571834e
hsinyilow <hsinyilow@gmail.com>
2025-01-30 22:18:08 +0800

    Refactor code to follow OOP rules
    Not all classes follow OOP rules.
    
    Better for classes to follow OOP for code reusability and flexibility.
    
    Let's:
    *Change Task.java to abstract class
    *Add Ui, Parser, TimeData classes
    *Move functions to appropriate classes

--- src/main/java/SaveSystem.java
+++ src/main/java/SaveSystem.java
@@ -11,66 +11,66 @@ public class SaveSystem {
     private static boolean write = false;
     private static List<String> allLines = new ArrayList<>();
 
-    public static void Load(){
-        filePath = Paths.get("data", "astraData.txt");
-        if (Files.exists(filePath)){
-            //load
+    public static void Load(String path, TaskList taskList) {
+        filePath = Paths.get(path);
+
+        if (Files.exists(filePath)) {
+            //load save file
             try {
                 allLines = Files.readAllLines(filePath);
 
                 for (int i = 0; i < allLines.size(); i++) {
-                    //run the thing
-                    TodoList.AddTask(allLines.get(i));
+                    taskList.AddTask(allLines.get(i));
                 }
             } catch (IOException e) {
-                System.out.println("Error in reading a save file. Please try again later");
+                Ui.FeedbackError("Error in loading a save file.");
             }
 
         } else {
+            //create new file
             try {
                 Files.createFile(filePath);
             } catch (IOException e) {
-                System.out.println("Error in creating a save file. Please try again later");
+                Ui.FeedbackError("Error in creating a save file.");
             }
         }
 
         write = true;
-
     }
 
     //append new items
     public static void Add(String data){
         if (!write) return;
 
-        try(FileWriter fileWriter = new FileWriter(filePath.toString(), true)) {
+        try (FileWriter fileWriter = new FileWriter(filePath.toString(), true)) {
             fileWriter.append(data);
             fileWriter.append(System.lineSeparator());
             allLines.add(data);
         } catch (IOException e) {
-            System.out.println(e);
+            Ui.FeedbackError("Error in saving a task.");
         }
     }
 
-    //update status (mark)
+    //update changes
     public static void Update(int index, String data){
         if (!write) return;
         allLines.set(index, data);
         try {
             Files.write(filePath, allLines);
         } catch (IOException e) {
-            System.out.println(e);
+            Ui.FeedbackError("Error in saving the changes.");
         }
 
     }
 
     //delete task from save file
     public static void Delete(int index) {
-        if(!write) return;
+        if (!write) return;
         allLines.remove(index);
         try {
             Files.write(filePath, allLines);
         } catch (IOException e) {
-            System.out.println(e);
+            Ui.FeedbackError("Error in saving the changes.");
         }
     }
 }
--- src/main/java/TodoTask.java
+++ src/main/java/TodoTask.java
@@ -1,26 +1,29 @@
-public class TodoTask extends Task{
+public class TodoTask extends Task {
 
-    public TodoTask(String input) throws AstraException{
-        super();
-
-        if(input.startsWith("T ")) {
-            String[] parseInput = input.split(" \\Q|\\E ");
+    public TodoTask(String input) throws AstraException {
+        if (input.startsWith("T ")) {
+            //load save
+            String[] parseInput = Parser.ParseSaveFile(input);
             this.description = parseInput[2];
             this.done = parseInput[1].equals("true");
 
         } else {
-            String result = commandCheck(input, 4);
+            //add task
+            String result = Parser.ParseCommand(input, 4, false);
             if (result.isEmpty()){
                 throw new AstraException("Invalid task description");
             }
             this.description = result;
         }
-
-
     }
 
     @Override
     protected String saveString(){
-        return "T | " + done + " | " + description;
+        return String.format("T | %b | %s", done, description);
+    }
+
+    @Override
+    public String displayTask(){
+        return String.format("[T][%s] %s", (done? "X" : " "), description);
     }
 }
