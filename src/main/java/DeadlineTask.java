41a6861c7d692802b4cece498b4ad247fd0518df
hsinyilow <hsinyilow@gmail.com>
2025-01-30 16:55:29 +0800

    Update tasks to recognise date and time command
    Deadline and Event tasks recognise everything as a string.
    
    Date and time recognising is more useful for future functionalities.
    
    Let's,
    * update the parser to recognise date and time
    * save date and time as LocalDateTime

--- src/main/java/DeadlineTask.java
+++ src/main/java/DeadlineTask.java
@@ -1,15 +1,22 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class DeadlineTask extends Task{
-    protected String deadline;
+    protected LocalDateTime deadline;
+    protected boolean keepTime = false;
+
     public DeadlineTask(String input) throws AstraException{
 
         if(input.startsWith("D ")) {
             String[] parseInput = input.split(" \\Q|\\E ");
             this.description = parseInput[2];
             this.done = parseInput[1].equals("true");
-            this.deadline = parseInput[3];
+            this.deadline = LocalDateTime.parse(parseInput[3]);
         } else {
             String[] parseInput = input.split("/");
-            if(parseInput.length != 2 || !parseInput[1].startsWith("by")){
+            if(parseInput.length != 2){
                 throw new AstraException("Invalid Deadline Task command");
             }
 
@@ -23,7 +30,15 @@ public class DeadlineTask extends Task{
             }
 
             this.description = descriptionResult;
-            this.deadline = deadlineResult;
+            parseInput = deadlineResult.split(" ");
+            LocalDate date = LocalDate.parse(parseInput[0]);
+            LocalTime time = LocalTime.MIN;
+            if (parseInput.length != 1){
+                time = LocalTime.of(Integer.parseInt(parseInput[1].substring(0,2)),
+                        Integer.parseInt(parseInput[1].substring(2)));
+                keepTime = true;
+            }
+            deadline = LocalDateTime.of(date, time);
         }
 
 
@@ -31,7 +46,15 @@ public class DeadlineTask extends Task{
 
     @Override
     public String displayTask() {
-        return String.format("[D][%s] %s (by: %s)", (done? "X" : " "), description, deadline);
+
+        return String.format("[D][%s] %s (by: %s)", (done? "X" : " "), description, getDeadline(keepTime));
+
+    }
+
+    public String getDeadline(boolean withTime){
+        String time = deadline.toLocalTime().format(DateTimeFormatter.ofPattern("hh:mm a"));
+        String date = deadline.toLocalDate().format(DateTimeFormatter.ofPattern("dd MMMM yyyy"));
+        return withTime? String.format("%s %s", date, time) : String.format("%s", date);
     }
 
     @Override
